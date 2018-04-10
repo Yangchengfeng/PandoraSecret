cf5828521f999dbd18ee9baa00e8536cc9d033df
yangchengfeng <linmeinicoder@gmail.com>
2018-04-10 22:44:51 +0800

    启动页面添加视频

--- PandoraSecret/PandoraSecret/Classes/Launch/Controller/PSLaunchViewController.m
+++ PandoraSecret/PandoraSecret/Classes/Launch/Controller/PSLaunchViewController.m
@@ -8,6 +8,13 @@
 
 #import "PSLaunchViewController.h"
 #import "PSMainTabBarController.h"
+#import <AVKit/AVKit.h>
+
+@interface PSLaunchViewController ()
+
+@property (nonatomic, strong) AVPlayerViewController *avPlayer;
+
+@end
 
 @implementation PSLaunchViewController
 
@@ -19,15 +26,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setMoviePlayer];
+}
+
+-(void)setMoviePlayer{
+    
+    _avPlayer = [[AVPlayerViewController alloc]init];
+    self.avPlayer.allowsPictureInPicturePlayback = NO;
+    self.avPlayer.showsPlaybackControls = NO;
+    
+    NSString *path =  [[NSBundle mainBundle] pathForResource:@"LaunchTour.mp4" ofType:nil];
+    AVPlayerItem *item = [[AVPlayerItem alloc] initWithURL:[NSURL fileURLWithPath:path]];
+    AVPlayer *player = [AVPlayer playerWithPlayerItem:item];
+    AVPlayerLayer *layer = [AVPlayerLayer playerLayerWithPlayer:player];
+    [layer setFrame:[UIScreen mainScreen].bounds];
+    layer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _avPlayer.player = player;
+    //添加到self.view上面去
+    [self.view.layer insertSublayer:layer atIndex:0];
+    //开始播放
+    [_avPlayer.player play];
+    
+    // 重复播放
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playDidEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:item];
+    
+}
+
+
+//播放完成的代理
+- (void)playDidEnd:(NSNotification *)Notification{
+    [_avPlayer.player seekToTime:CMTimeMake(0, 1)];
+    //开始播放
+    [_avPlayer.player play];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
     self.navigationController.navigationBar.hidden = NO;
-}
-
-- (IBAction)pushToMain:(id)sender {
-    [self.navigationController pushViewController:[[PSMainTabBarController alloc] init] animated:YES];
 }
 
 - (BOOL)prefersStatusBarHidden {
